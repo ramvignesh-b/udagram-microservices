2766d20f12213b60c7fea5099b7f19c68610da40
RamVignesh B <ramvigneshgreat@pec.edu>
2020-05-14 19:50:19 +0530

    add

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=udagramram
+export POSTGRES_PASSWORD=udagramram
+export POSTGRES_HOST=udagramram.ctt8dwarwby2.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=udagramram
+export AWS_BUCKET=udagramram-dev
+export AWS_REGION=ap-south-1
+export AWS_PROFILE=default
+export JWT_SECRET=RAMVIGNESH
 export URL=http://localhost:8100
